afd0f45e48e9c0f404bd0b805cc8ea97a8ff2a04
Chawalit V <vchawalit@gmail.com>
2018-03-21 17:29:14 +0700

    fix
    fix

--- bot_settings.php
+++ bot_settings.php
@@ -1,6 +1,6 @@
 <?php
 /// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
-define('LINE_MESSAGE_CHANNEL_ID','กรอก ค่า Channel ID');
-define('LINE_MESSAGE_CHANNEL_SECRET','กรอกค่า Channel secret');
-define('LINE_MESSAGE_ACCESS_TOKEN','กรอกค่า Channel access token');
+define('LINE_MESSAGE_CHANNEL_ID','1522274158');
+define('LINE_MESSAGE_CHANNEL_SECRET','137c3ab62931fe74e5d7a1da381da15c');
+define('LINE_MESSAGE_ACCESS_TOKEN','LvT9xy2S/PpEYV13/nYmuiKPmCN32IgDIedHiPx/Yblb/o56FfA1O/jm5nQPkWfLlbGe0+qqULp/RcnNz6lYX6VVX22NejRzzxfPINneai29WceiQFJ04BMUhSLeJoJd/ffZW+lFzjwFudtrgD1FBQdB04t89/1O/w1cDnyilFU=');
 ?>
